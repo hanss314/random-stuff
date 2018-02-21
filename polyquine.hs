090f22ff22752290c740670c76745e576e558a99
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-02-21 14:42:28 -0800

    Use more null characters

--- polyquine.hs
+++ polyquine.hs
@@ -1,4 +1,4 @@
 py="--0; print('py='+repr(py)+'\\nhs='+repr(hs)+'\\n'+py+hs)\n"
-hs="1#1=0; main = putStrLn (['p','y','='] ++ show py ++ ['\\n','h','s'] ++ show hs ++ ['\\n'] ++ py ++ hs)"
+hs="0#_=0; main = putStrLn (['p','y','='] ++ show py ++ ['\\n','h','s'] ++ show hs ++ ['\\n'] ++ py ++ hs)"
 --0; print('py='+repr(py)+'\nhs='+repr(hs)+'\n'+py+hs)
-1#1=0; main = putStrLn (['p','y','='] ++ show py ++ ['\n','h','s'] ++ show hs ++ ['\n'] ++ py ++ hs)
+0#_=0; main = putStrLn (['p','y','='] ++ show py ++ ['\n','h','s'] ++ show hs ++ ['\n'] ++ py ++ hs)
